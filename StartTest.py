48d2f2f8ceadf339950711796d747dbdf069cf78
Alvin <lai.fu@icloud.com>
2021-07-02 23:33:42 +0800

    change

--- StartTest.py
+++ StartTest.py
@@ -1,4 +1,6 @@
+print "testnode"
 execfile('D:/forwindows/testnode.py')
+print "getDataSource"
 execfile('D:/forwindows/getDatasourcelist.py')
 
-execfile('D:/forwindows/getDatasourcelist.py')
+#execfile('D:/forwindows/getDatasourcelist.py')
